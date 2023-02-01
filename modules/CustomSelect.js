53169f1d836d4706f9f9091a860bdee33a47d9c3
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-02-01 07:32:25 +0500

    deleted get and set

--- modules/CustomSelect.js
+++ modules/CustomSelect.js
@@ -12,33 +12,13 @@ class CustomSelect {
         this.#isActive = false
         this.#currentSelectedOption = null
     }
-    
-    get #getId() {
-        return this.#id
-    }
-
-    get #getOptions() {
-        return this.#options
-    }
-
-    set #setIsActive(value) {
-        this.#isActive = value
-    }
-
-    get #getIsActive() {
-        return this.#isActive
-    }
-
-    set #setSelectedValue(value) {
-        this.#currentSelectedOption = value
-    }
 
     get #selectedValue() {
         return this.#currentSelectedOption
     }
 
     render(container) {
-        const dropdown = createDropdown(this.#getId, this.#getOptions)
+        const dropdown = createDropdown(this.id, this.#options)
         container.append(dropdown)
         
         const dropdownButton = container.querySelector('.select-dropdown__button')
@@ -46,16 +26,16 @@ class CustomSelect {
         const nodeText = container.querySelector('.select-dropdown__text') 
         
         const switchList = () => {
-            if (!this.#getIsActive) dropdownList.classList.add('active')       
+            if (!this.#isActive) dropdownList.classList.add('active')       
             else dropdownList.classList.remove('active')
-            this.#setIsActive = !this.#getIsActive
+            this.#isActive = !this.#isActive
         }
 
         const handlerClick = (event) => {
             const clickedItem = event.target.closest('.select-dropdown__list-item')
             clickedItem.classList.add('selected')
             const itemDataValue = clickedItem.dataset.value
-            this.#setSelectedValue = this.#getOptions.filter(item => item.value == itemDataValue)[0]
+            this.#currentSelectedOption = this.#options.filter(item => item.value == itemDataValue)[0]
             nodeText.innerText = this.#selectedValue.text
             switchList()
         }    
